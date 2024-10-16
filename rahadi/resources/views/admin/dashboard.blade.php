6e0a50ff040cee9eb7bf1f71ece3d9a8a4486e0c
Rahadirahman21 <rahadi.rahman2007@smkn4-tsm.sch.id>
2024-10-17 03:13:27 +0700

    siswa

--- rahadi/resources/views/admin/dashboard.blade.php
+++ rahadi/resources/views/admin/dashboard.blade.php
@@ -11,6 +11,7 @@
     <h1>Dashboard Admin</h1>
 
     <!-- Logout Link -->
+     <a class="nav-link" href="{{ route('siswa.index')}}">Data Siswa</a>
     <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
 
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
--- rahadi/routes/web.php
+++ rahadi/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginRegisterController;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\SiswaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +20,7 @@ Route::middleware('guest')->group(function () {
 // Group routes untuk yang sudah login dan role admin
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+    route::resource('/admin/siswa',SiswaController::class);
     Route::post('/logout', [LoginRegisterController::class, 'logout'])->name('logout');
 });
 
